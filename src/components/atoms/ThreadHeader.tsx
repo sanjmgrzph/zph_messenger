674ead3a098c06601c65de4c4242648670fd312a
sanjaymagarzph <sanjay.magar@zakipoint.com>
2022-08-01 13:15:27 +0545

    changes select icon

--- src/components/atoms/ThreadHeader.tsx
+++ src/components/atoms/ThreadHeader.tsx
@@ -76,12 +76,12 @@ export default function ThreadHeader() {
                 <button type="button" className="p-2 -m-2 ml-5 sm:ml-7 text-gray-400 hover:text-gray-500"
                         onClick={() => setIsBulkRemove()}>
                     <span className="sr-only">Bulk Remove</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-select" width="24"
-                         height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#9e9e9e" fill="none"
-                         strokeLinecap="round" strokeLinejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-square-check"
+                         width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9e9e9e" fill="none"
+                         stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <rect x="4" y="4" width="16" height="16" rx="2"/>
-                        <path d="M9 11l3 3l3 -3"/>
+                        <path d="M9 12l2 2l4 -4"/>
                     </svg>
                 </button>
 
